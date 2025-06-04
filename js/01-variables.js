05948d2ffc203c960fe1e64f30d73d7e49d541eb
laudecima <laudecima0@gmail.com>
2025-06-03 21:32:18 -0300

    practicamos js variables

---
+++ js/01-variables.js
@@ -0,0 +1,16 @@
+// comentario de una sola linea
+
+/*
+esto es un comentario de 
+multiples lineas
+*/
+
+// crear una variable (let - const - var)
+
+let numero1;
+
+let anio = 2025
+
+//mostrar valor de las variables en javascript
+console.log('Hola mundo')
+console.log(numero1, anio)
