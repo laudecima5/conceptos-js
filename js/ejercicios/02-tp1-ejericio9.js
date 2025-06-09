ca32a0e5c14f196982f3f2a0fb142da0dd4e4600
laudecima <laudecima0@gmail.com>
2025-06-09 18:38:33 -0300

    realizamos el ejercicio 9 del tp1

---
+++ js/ejercicios/02-tp1-ejericio9.js
@@ -0,0 +1,10 @@
+//escribe un programa que pida una frase y escriba las vocales que aparecen
+//nota: tener en cuenta la funcion length y substring o charat (developer mozilla)
+
+let frase = prompt('Ingresa una frase')
+//hola mundo
+console.log(frase) 
+console.log(frase.length) 
+console.log(frase.substring(0,3)) 
+console.log(frase.charAt(0))
+console.log(frase.charAt(5))
